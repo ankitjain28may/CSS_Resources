2dbdda9ad5aa972118d6709d580af6c8cec00eea
ankitjain28may <ankitjain28may77@gmail.com>
2016-09-10 11:25:52 +0530

    Fix bug of NaN

--- Progress Bar/js/index.js
+++ Progress Bar/js/index.js
@@ -3,26 +3,32 @@ function move()
 	var elem = document.getElementById("myBar");
 	var lb=document.getElementById("label");
 	var val=document.getElementById("inp").value;
-		var width = 0;
-  if(val >100)
+    var width = 0;
+    if(val >100)
     {
- document.getElementById("error").innerHTML="Incorrect, Your value is greater than 100";
+        document.getElementById("error").innerHTML="Incorrect, Your value is greater than 100";
     }
-  else
+    else if(isNaN(val))
     {
-     if(val==0)
-     elem.style.width = width + '%'; document.getElementById("error").innerHTML="";
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width == val) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-
+        document.getElementById("error").innerHTML="Incorrect, You cant enter alphabets";
     }
-  }
+    else
+    {
+        if(val==0)
+            elem.style.width = width + '%';
+            document.getElementById("error").innerHTML="";
+        var id = setInterval(frame, 10);
+        function frame()
+        {
+            if (width == val) {
+                clearInterval(id);
+            }
+            else {
+                width++;
+                elem.style.width = width + '%';
+            }
+        }
 
 		lb.innerHTML=val*1+'%';
-}
+    }
 }
